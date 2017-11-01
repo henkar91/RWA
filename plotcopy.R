ba16adc710babbe6de8dcb0dbbaac8a374b49be3
ogge sjölin <ogge@tuaril.org>
2017-11-01 12:48:08 +0100

    Added arrow line, colorgroups

--- plotcopy.R
+++ plotcopy.R
@@ -1,4 +1,7 @@
 library(tidyverse)
+
+# Build dummy data --------------------------------------------------------
+
 df <- data_frame(country = c(rep("SE", 10), rep("NO", 10)),
                  importance = runif(20, 0, 1),
                  performance = runif(20, .3, .9),
@@ -7,18 +10,29 @@ df <- data_frame(country = c(rep("SE", 10), rep("NO", 10)),
                               'statement9','statement10'),2))
 
 
-library(ggthemes)
-ggplot(df, aes(x = importance, y = performance * 100)) + geom_point() +
-    geom_text(aes(x = importance, y = performance * 100, label = itemtext), 
+# Add colorgroup on perf/imp ----------------------------------------------
+
+df <- df %>% 
+    mutate(colorgroup = case_when(importance < 0.5 & performance < 0.5 ~ "ll",
+                                  importance < 0.5 & performance > 0.5 ~ "ul",
+                                  importance > 0.5 & performance < 0.5 ~ "lr",
+                                  importance > 0.5 & performance > 0.5 ~ "ur"))
+
+
+# Try to mimic Timmys plot ------------------------------------------------
+
+ggplot(df, aes(x = importance, y = performance * 100, color = colorgroup)) + geom_point() +
+    geom_text(aes(x = importance, y = performance * 100, label = itemtext, color = colorgroup), 
                   vjust = -.8) +
+    guides(color = FALSE) +
     theme_minimal() +
     theme(plot.margin = unit(c(1,1,1,1), "cm"),
           panel.grid.major.y = element_blank(),
           panel.grid.minor.y = element_blank(),
           panel.grid.major.x = element_blank(),
           panel.grid.minor.x = element_blank(),
-          axis.line.x = element_line(color = "grey50"),
-          axis.line.y = element_line(color = "grey50"),
+          axis.line.x = element_blank(),
+          axis.line.y = element_blank(),
           axis.text.x=element_blank(),
           axis.text.y = element_text(color="grey", size="10"),
           axis.title.x = element_text(color="#4c7a9f", size="10", face="bold"),
@@ -28,15 +42,16 @@ ggplot(df, aes(x = importance, y = performance * 100)) + geom_point() +
           legend.position="bottom", legend.title = element_blank(),
           legend.key = element_blank(),
           legend.text = element_text(color="#2e6e9c", size="10")) +
-    # scale_x_continuous(expand = c(0, 0)) + # Howto get 0 at absolute 0?
-    # scale_y_continuous(expand = c(0, 0)) +
-    ylim(c(0,100)) +
+    scale_x_continuous(limits = c(0,1)) +
+    scale_y_continuous(limits = c(0,100)) +
     xlab(label = "IMPORTANCE (IMPACT ON OVERALL STORE SATISFACTION)") +
     ylab(label = "PERFORMANCE (% SCORE 4-5)") +
     annotate(geom = "text", x = 0.02, y = 2, label = "Low importance", 
              color = "#4c7a9f", size = 3, fontface = "italic", hjust = 0) +
     annotate(geom = "text", x = 0.98, y = 2, label = "High importance", 
              color = "#4c7a9f", size = 3, fontface = "italic", hjust = 1) +
-    geom_rect(mapping=aes(xmin=0.49, xmax=0.51, ymin=0, ymax=100), fill = "grey90", alpha=0.01) +
-    geom_rect(mapping=aes(xmin=0, xmax=1, ymin=48, ymax=52), fill = "grey90", alpha=0.01)
+    annotate("segment", x=-Inf, xend = Inf, y = 0, yend = 0, arrow = arrow(), color = "grey")+
+    # scale_y_continuous(limits=c(-0.5, 1)) +
+    geom_rect(mapping=aes(xmin=0.49, xmax=0.51, ymin=0, ymax=100), color = NA, fill = "grey98", alpha=0.01) +
+    geom_rect(mapping=aes(xmin=0, xmax=1, ymin=48, ymax=52), color = NA, fill = "grey90", alpha=0.01)
 
