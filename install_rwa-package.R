60582cc738e5c8866b50934df556095fe151662c
Henrik Karlsson <henkar91@gmail.com>
2018-08-28 13:14:03 +0200

    Added plotting function, rwa_plot()

--- install_rwa-package.R
+++ install_rwa-package.R
@@ -6,10 +6,15 @@ library(rwa)
 data("exdata")
 
 # Test with split
-rwa(formula = "dependant ~ q1+q2+q3+q4+q5+q6+q7+q8+q9", data = df, split_var = "split", weights = "weight", tb_limit = 4)
+res <- rwa(formula = "dependant ~ q1+q2+q3+q4+q5+q6+q7+q8+q9", data = df, split_var = "split", weights = "weight", tb_limit = 4)
+res
+rwa_plot(res)
 
 # Test without split
 rwa(formula = "dependant ~ q1+q2+q3+q4+q5+q6+q7+q8+q9", data = df, weights = "weight", tb_limit = 4)
+
+# Test without split without weight
+# rwa(formula = "dependant ~ q1+q2+q3+q4+q5+q6+q7+q8+q9", data = df, tb_limit = 4)
 
 data2 <- data
 data2[c(1,2,10), 4] <- NA
---
+++ rwa/R/rwa_plot.R
@@ -0,0 +1,25 @@
+#' @title RWA plot
+#' @name rwa_plot
+#' @param rwa_object list, output from function rwa()
+#' @return A list of plots
+#' @description Makes a scatterplot of relative weight analysis for each split
+#' @export
+
+rwa_plot <- function(rwa_object){
+    nIter <- ncol(rwa_object$rwa)
+    
+    output <- list()
+    for(i in 1:nIter){
+        output[[i]] <- eval(substitute(ggplot() +
+            geom_point(aes(x = rwa_object[[1]][, i], y = rwa_object[[2]][, i])) +
+            geom_vline(aes(xintercept = mean(rwa_object[[1]][, i]))) +
+            geom_hline(aes(yintercept = mean(rwa_object[[2]][, i]))) +
+            theme_minimal() +
+            ggtitle(label = paste("Relative Weight Analysis", colnames(rwa_object[[1]][i]), sep = " "),
+                    subtitle = paste(expression(R2), ": ", round(rwa_object[[3]][i], 2), "\n",
+                    "Base: ", rwa_object[[4]][i], sep = "")) +
+            labs(x = "Relative Weight", y = "Share of Top Box") +
+            scale_y_continuous(labels = scales::percent), list(i=i)))
+    }
+    return(output)
+}
